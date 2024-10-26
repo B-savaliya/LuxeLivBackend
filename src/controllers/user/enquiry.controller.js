3ca432238f8a0251c12015e5b159256cb27ad97a
Jenish Ghadiya <jenishvghadiya@gmail.com>
2024-10-26 15:29:57 +0530

    classic

--- src/controllers/user/enquiry.controller.js
+++ src/controllers/user/enquiry.controller.js
@@ -35,10 +35,10 @@ export default {
                 </head>
                 <body>
                     <div class="container">
-                        <div class="header">
+                        <div class="header" style="border: 1px solid #fff; border-bottom: none;">
                             <h1>Welcome to Classic Motel</h1>
                         </div>
-                        <div class="content" style="color: #53624e;">
+                        <div class="content" style="color: #53624e; border-x: 1px solid #fff; ">
                             <p>Dear ${name},</p>
                             <p>Thank you for your inquiry. We're delighted to welcome you to Classic Motel!</p>
                             <p>We have received your message and our team will get back to you as soon as possible.</p>
@@ -51,7 +51,7 @@ export default {
                             <p>We look forward to serving you and ensuring your stay at Classic Motel is nothing short of exceptional.</p>
                             <a href="https://classicmotel.com" class="btn">Visit Our Website</a>
                         </div>
-                        <div class="footer">
+                        <div class="footer" style="border: 1px solid #fff; border-top: none;">
                             <p>Best regards,<br>The Classic Motel Team</p>
                         </div>
                     </div>
